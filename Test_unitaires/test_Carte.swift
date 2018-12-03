b86c0d40ea9276f6703ccdb0a99497c85a65d902
Laurancy-Dorian <laurancy.dorian@gmail.com>
2018-12-03 09:57:04 +0100

    Test carte

--- Test_unitaires/test_Carte.swift
+++ Test_unitaires/test_Carte.swift
@@ -21,8 +21,8 @@ func test_init() -> Int {
 
 func test_init2() {
 
-  print ("== Test de l'init(String x Int x Int x Int x (Int,Int)[] ) ==");
-  var portee : [(Int,Int)] = [(1,2), (0,0)];
+  print ("== Test de l'init(String x Int x Int x Int x (Int,Int)[]) ==");
+  var portee : [(Int,Int)] = [(1,2), (0,1)];
   var porteeVide = [];
 
   // Carte valide
@@ -92,12 +92,12 @@ func test_init2() {
 func test_puissance_attaque() {
   print ("== Test de puissance_attaque() ==");
 
-  var portee : [(Int,Int)] = [(1,2), (0,0)];
+  var portee : [(Int,Int)] = [(1,2), (0,1)];
   // Carte valide
   do {
     try var c1 : CarteProtocol = Carte("Soldat", 3, 4, 3, portee);
     if (c1.puissance_attaque() != 3) {
-      print ("Test ko : la puissance d'attaque ne renvoie pas le bonne valeur")
+      print ("Test ko : la puissance d'attaque ne renvoie pas la bonne valeur")
       return 0;
     }
     else {
@@ -116,11 +116,97 @@ func test_puissance_attaque() {
     print ("Test ok : valeur negative puissance attaque");
   }
 
-  print ("== Fin test init() ==");
+  print ("== Fin test puissance_attaque() ==");
+  return 1;
+}
+
+func test_pv_defensif() {
+  print ("== Test de pv_defensif() ==");
+
+  var portee : [(Int,Int)] = [(1,2), (0,1)];
+  // Carte valide
+  do {
+    try var c1 : CarteProtocol = Carte("Soldat", 3, 4, 3, portee);
+    if (c1.pv_defensif() != 4) {
+      print ("Test ko : pv_defensif ne renvoie pas la bonne valeur")
+      return 0;
+    }
+    else {
+      print ("Test ok : puissance attaque correcte")
+    }
+  } catch {
+    print ("Erreur d'init : Une carte valide a renvoye une exception");
+    return 0;
+  }
+
+  do {
+    try var c2 : CarteProtocol = Carte("Soldat", 4, -1, 3, portee);
+    print ("Test ko : Les pv_defensif ne peuvent pas etre negatifs");
+    return 0;
+  } catch {
+    print ("Test ok : valeur negative pv_defensif");
+  }
+
+  do {
+    try var c3 : CarteProtocol = Carte("Soldat", 4, 0, 3, portee);
+    print ("Test ko : Les pv_defensif ne peuvent pas etre nuls");
+    return 0;
+  } catch {
+    print ("Test ok : valeur nulle pv_defensif");
+  }
+
+  do {
+    try var c4 : CarteProtocol = Carte("Soldat", 4, 3, 5, portee);
+    print ("Test ko : Les pv_defensif ne peuvent pas etre etre inferieurs aux pv offensifs");
+    return 0;
+  } catch {
+    print ("Test ok : pv_defensif < pv_offensif");
+  }
+
+  print ("== Fin test pv_defensif() ==");
   return 1;
 }
 
 
+
+func test_pv_offensif() {
+  print ("== Test de pv_offensif() ==");
+
+  var portee : [(Int,Int)] = [(1,2), (0,1)];
+  // Carte valide
+  do {
+    try var c1 : CarteProtocol = Carte("Soldat", 3, 4, 3, portee);
+    if (c1.pv_offensif() != 3) {
+      print ("Test ko : pv_offensif ne renvoie pas la bonne valeur")
+      return 0;
+    }
+    else {
+      print ("Test ok : puissance attaque correcte")
+    }
+  } catch {
+    print ("Erreur d'init : Une carte valide a renvoye une exception");
+    return 0;
+  }
+
+  do {
+    try var c2 : CarteProtocol = Carte("Soldat", 4, 3, -1, portee);
+    print ("Test ko : Les pv_offensif ne peuvent pas etre negatifs");
+    return 0;
+  } catch {
+    print ("Test ok : valeur negative pv_offensif");
+  }
+
+  do {
+    try var c3 : CarteProtocol = Carte("Soldat", 4, 2, 0, portee);
+    print ("Test ko : Les pv_offensif ne peuvent pas etre nuls");
+    return 0;
+  } catch {
+    print ("Test ok : valeur nulle pv_offensif");
+  }
+
+  print ("== Fin test pv_offensif() ==");
+  return 1;
+}
 
 // ==== Tests ====
 var nb_test_ok : Int = 0;
@@ -131,3 +217,9 @@ nb_test_ok += test_init();
 
 nb_test_tot += 1;
 nb_test_ok += test_init2();
+
+nb_test_tot += 1;
+nb_test_ok += test_puissance_attaque();
+
+nb_test_tot += 1;
+nb_test_ok += test_pv_defensif();
